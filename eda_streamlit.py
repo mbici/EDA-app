7d71439310db28efa7180f213e4787223666e067
Mbici <gitaulawrence23@gmail.com>
2023-03-22 13:02:05 +0300

    Added code files and requirements

---
+++ eda_streamlit.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from pandas_profiling import ProfileReport
+import streamlit as st
+from streamlit_pandas_profiling import st_profile_report
+st.title("A Simple WebApp to do Basic DataExploration")
+file_type = st.selectbox("What type of file do you want to analyze?",['csv','excel',""])
+uploaded_data=st.file_uploader("Kindly upload your dataset")
+if uploaded_data:
+    if file_type == 'csv':
+        data = pd.read_csv(uploaded_data)
+    elif file_type == 'excel':
+        data = pd.read_excel(uploaded_data)
+    data_report = ProfileReport(data,explorative=True)
+    st_profile_report(data_report)
